9a1e92ea746730976e08926783fc20cd4d7fc560
Keith Henderson <keith.donaldh@gmail.com>
2025-08-04 20:35:29 -0400

    update

--- nvim/lua/custom/plugins/nvim-lspconfig.lua
+++ nvim/lua/custom/plugins/nvim-lspconfig.lua
@@ -178,6 +178,7 @@ return {
 
     local ensure_installed = vim.tbl_keys(servers or {})
     vim.list_extend(ensure_installed, {
+      'clang-format',
       'stylua',
       'google-java-format',
       'black',
