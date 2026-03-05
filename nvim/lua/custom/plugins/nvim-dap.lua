077f9c00f465626ef7431ee1323e60dec272a7ef
Keith Henderson <keith.donaldh@gmail.com>
2026-03-05 14:59:48 -0500

    update

--- nvim/lua/custom/plugins/nvim-dap.lua
+++ nvim/lua/custom/plugins/nvim-dap.lua
@@ -11,14 +11,44 @@ return {
       local dap = require 'dap'
       local ui = require 'dapui'
 
+      -- Setup DAP UI
       ui.setup()
 
+      -- Configure codelldb Adapter
       dap.adapters.codelldb = {
         type = 'executable',
         command = vim.fn.expand '~/.local/share/nvim/mason/packages/codelldb/extension/adapter/codelldb',
-        -- Remove args entirely - codelldb will pick an available port
         options = {
           initialize_timeout_sec = 20,
+        },
+      }
+
+      -- *** CORRECTED: Add Rust Configuration ***
+      dap.configurations.rust = {
+        {
+          type = 'codelldb',
+          request = 'launch',
+          name = 'Run program',
+          program = function()
+            -- 1. Get absolute path of the current file
+            local file = vim.fn.expand '%:p'
+
+            -- 2. Get the directory of the current file
+            local file_dir = vim.fn.fnamemodify(file, ':h')
+
+            -- 3. Get the PROJECT ROOT (parent of src directory)
+            -- This is where Cargo.toml lives, and where 'target/' is created
+            local project_root = vim.fn.fnamemodify(file_dir, ':h')
+
+            -- 4. Get the PROJECT NAME (This replaces 'main' with 'hello_world')
+            -- For 'cargo new', the project folder name matches the binary name
+            local project_name = vim.fn.fnamemodify(project_root, ':t')
+
+            -- 5. Construct the full path
+            return project_root .. '/target/debug/' .. project_name
+          end,
+          cwd = vim.fn.getcwd(),
+          args = {},
         },
       }
 
